6ba3f14178e05bf4d4b9fddad4f888ba22b7e80c
AnAfterthought <chngweicheng@gmail.com>
2024-06-21 23:30:49 +0800

    feat: Navigation bars based on login status

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -1,18 +1,29 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { selectUser } from "../features/manageAccount";
 
 function NavBar() {
-	return (
-		<nav>
-			<NavLink to="/">Home</NavLink>
-			<NavLink to="/login">Login</NavLink>
-			<NavLink to="/register">Register</NavLink>
-			<NavLink to="/deck">Deck</NavLink>
-			<NavLink to="/flashcards">Flashcards</NavLink>
-			<NavLink to="/ocr">OCR</NavLink>
-			<NavLink to="/logout">Logout</NavLink>
-		</nav>
-	)
+
+	const isLoggedIn = useSelector(selectUser);
+
+	if (!isLoggedIn) {
+		return (
+			<nav>
+				<NavLink to="/login">Login</NavLink>
+				<NavLink to="/register">Register</NavLink>
+			</nav>
+		)
+	} else {
+		return (
+			<nav>
+				<NavLink to="/deck">Deck</NavLink>
+				<NavLink to="/flashcards">Flashcards</NavLink>
+				<NavLink to="/ocr">OCR</NavLink>
+				<NavLink to="/logout">Logout</NavLink>
+			</nav>
+		)
+	}
 }
 
 export default NavBar;
